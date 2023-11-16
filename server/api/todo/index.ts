396446b0e377c1f3688474173bd70a3fdecf94d3
ardalan.shahandeh <ardalan.shahandeh@gmail.com>
2023-11-16 19:16:14 +0330

    POST End Point Request

--- server/api/todo/index.ts
+++ server/api/todo/index.ts
@@ -1,6 +1,7 @@
 import { db } from "../../db";
+import { v4 as uuid } from "uuid";
 
-export default defineEventHandler((e) => {
+export default defineEventHandler(async (e) => {
   console.log({ e });
   const method = e.req.method;
 
@@ -9,6 +10,19 @@ export default defineEventHandler((e) => {
   }
 
   if (method === "POST") {
+    const body = await useBody(e);
+
+    if (!body.item) throw new Error();
+
+    const newTodo = {
+      id: uuid(),
+      item: body.item,
+      completed: false,
+    };
+
+    db.todos.push(newTodo);
+
+    return newTodo;
   }
 
   if (method === "PUT") {
